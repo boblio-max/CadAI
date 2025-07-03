cd9e7f982165622d600ae9a6cb1ec456592e06ec
boblio-max <nikhilmahankali56@gmail.com>
2025-07-02 19:42:47 -0700

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,13 @@
+import logging
+
+def setup_logger():
+    """
+    Sets up a basic logger for the application.
+    """
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
+    return logging.getLogger(__name__)
+
+logger = setup_logger()
